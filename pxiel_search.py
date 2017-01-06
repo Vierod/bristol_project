c7d18ed6581e16ace57becc683fa684851a8d11f
Tim Powell <tap1@aber.ac.uk>
2017-01-06 10:47:10 +0000

    changed to import image and run through each pixel.

--- pxiel_search.py
+++ pxiel_search.py
@@ -1,7 +1,36 @@
-for x in range(100):
-    for y in range range(50):
-        image[x,y]
+#
+# Script for reading and writing images
+#
+from scipy import misc
+import matplotlib.pyplot as plt
+import glob
+import numpy as np
+from PIL import Image
+#
+# Read in image to numpy array
+#
+image = misc.imread("IMAGE GOES HERE")
+#
+# Render image
+#
+#plt.imshow(image)
+#plt.show()
+#
+# Convert numpy array back to image and save
+#
+
+
+#New image that the tree pixels will be added to.
+new_image = np.zeros(image.shape)
+
+
+
+for x in range(image.shape[0]):
+    for y in range(image.shape[1]):
         if image[x,y] == [1,2,3]:
-            append.new_image = image[x,y]
+            new_image[x,y] = image[x,y]
         else:
-            append.new_image = [0,0,0]
+            pass
+
+#image_png = Image.fromarray(image,'RGB')
+#image_png.save('image.png')
