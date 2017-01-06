e794f5dad9ceaddb403f612bfcfc5711e9634468
Tim Powell <tap1@aber.ac.uk>
2017-01-06 13:38:02 +0000

    Tidied up code.

--- pixel_search.py
+++ pixel_search.py
@@ -1,5 +1,7 @@
 #
-# Script for reading and writing images
+# Script that takes reference images and uses the pixel values in these to 
+# apply a mask to an image. Here only pixel values that are present in the
+# reference images are kept. All other pixels are changed to black.
 #
 from scipy import misc
 import matplotlib.pyplot as plt
@@ -8,26 +10,20 @@ import numpy as np
 #
 # Read in image to numpy array
 #
-image = misc.imread("bristol_tree_1.jpg")
+image = misc.imread("bristol_tree_1.png")
 imageR1 = misc.imread("reference_images/bristol_reference_1.png")
 imageR2 = misc.imread("reference_images/bristol_reference_2.png")
 imageR = []
 #
-# Render image
-#
-#plt.imshow(image)
-#plt.show()
-#
-# Convert numpy array back to image and save
-#
 # New image that the tree pixels will be added to.
+
 new_image = np.zeros(image.shape)
 #
-# What to search the image for.
+# What to search the image for - keeping unique values.
 for i in range(imageR1.shape[0]):
     for j in range(imageR1.shape[1]):
         imageR.append(tuple(imageR1[i,j,:]))
-
+		
 for i in range(imageR2.shape[0]):
     for j in range(imageR2.shape[1]):
         imageR.append(tuple(imageR2[i,j,:]))
@@ -37,10 +33,11 @@ test = set(imageR)
 # Search through image for test pixels and put them into new_image
 for x in range(image.shape[0]):
     for y in range(image.shape[1]):
-        if tuple(image[x,y,:]) in test:
-            new_image[x,y] = image[x,y]
+        if tuple(image[x,y,:]) in imageR:
+            new_image[x,y,:] = image[x,y,:]
         else:
-            pass
+            new_image[x,y,:] = [255., 255., 255.]
 #
-# Write new image.
-misc.toimage(new_image, cmin=0.0, cmax=1.0).save('bristol_tree_output.png')
+# Write new image
+#
+misc.toimage(new_image, cmin=0.0, cmax=255.0).save('bristol_tree_output.png')
