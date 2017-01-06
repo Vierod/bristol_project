8f11aadc7518f7439fcb6b3c929e6f1b1a0b6ab3
Tim Powell <tap1@aber.ac.uk>
2017-01-06 10:16:52 +0000

    Tree image and first concept of pixel search script added.

---
+++ pxiel_search.py
@@ -0,0 +1,7 @@
+for x in range(100):
+    for y in range range(50):
+        image[x,y]
+        if image[x,y] == [1,2,3]:
+            append.new_image = image[x,y]
+        else:
+            append.new_image = [0,0,0]
